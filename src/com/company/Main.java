331336b6759eaf55429e89f086488e6425d3629b
yzz <yzz@gmail.com>
2021-09-26 22:40:08 +0800

    更新

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,8 @@ public class Main {
         System.out.println("hello");
         System.out.println("hello");
 	    System.out.println("hello  1");
+	    System.out.println("hello  1");
+	    System.out.println("hello  1");
 
     }
 }
